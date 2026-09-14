c2f563964c1f6d984d77e629bbabff49e1620eb0
Shah Jan Rasooli <shahjanrasooli51@gmail.com>
2026-09-14 09:23:08 +0430

    first Practice of Javav script

---
+++ TIME TABLE/app.js
@@ -0,0 +1,6 @@
+console.log("name: Shah Jan Rasooli");
+console.log("Father Name: AbdolALI");
+console.log("City: Mazr-e-Sharif");
+console.log("Date of end the course: 12/30/2026")
+console.log(800+200);
+console.log(["Sah Jan Rasooli", "Benafsha Amiri","Faiza Sultani"])
